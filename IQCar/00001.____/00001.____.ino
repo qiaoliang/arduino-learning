3151332629a612dda562e973670489140889550f
qiaoliang <qiaoliang.email@gmail.com>
2023-09-20 09:31:54 +0800

    add 智能小车练习“

---
+++ IQCar/00001.____/00001.____.ino
@@ -0,0 +1,14 @@
+void setup(){
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+
+}
